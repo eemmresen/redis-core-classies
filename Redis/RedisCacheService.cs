aec44274b6c8f792d8b1af3547f7ac4a1e7c9d24
eemmresen <eemmresen@gmail.com>
2024-02-20 08:40:10 +0300

    Add files via upload

---
+++ Redis/RedisCacheService.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+
+namespace CarrtellWebhook.Core.CacheService.Redis
+{
+    public class RedisCacheService
+    {
+        private RedisServer _redisServer;
+
+        public RedisCacheService()
+        {
+            _redisServer = new RedisServer();
+        }
+
+        public void Add(string key, object data,double second)
+        {
+            string jsonData = JsonConvert.SerializeObject(data);
+            _redisServer.Database.StringSet(key, jsonData);
+            _redisServer.Database.KeyExpire(key, DateTime.UtcNow.AddSeconds(second)); 
+        }
+
+        public bool Any(string key)
+        {
+            return _redisServer.Database.KeyExists(key);
+        }
+
+        public T Get<T>(string key)
+        {
+            if (Any(key))
+            {
+                string jsonData = _redisServer.Database.StringGet(key);
+                return JsonConvert.DeserializeObject<T>(jsonData);
+            }
+
+            return default;
+        }
+
+        public void Remove(string key)
+        {
+            _redisServer.Database.KeyDelete(key);
+        }
+
+        public void Close()
+        {
+            _redisServer.CloseDatabase();
+        }
+
+        public void Clear()
+        {
+            _redisServer.FlushDatabase();
+        }
+    }
+}
---
+++ Redis/RedisServer.cs
@@ -0,0 +1,40 @@
+﻿using StackExchange.Redis;
+
+namespace CarrtellWebhook.Core.CacheService.Redis
+{
+    public class RedisServer
+    {
+        private ConnectionMultiplexer _connectionMultiplexer;
+        private IDatabase _database;
+        private string configurationString;
+
+        private readonly string host = "your host id";
+        private readonly string port = "your port";
+        private readonly int databaseId = 0;
+
+        public RedisServer()
+        {
+            CreateRedisConfigurationString();
+
+            _connectionMultiplexer = ConnectionMultiplexer.Connect(configurationString);
+            _database = _connectionMultiplexer.GetDatabase(databaseId);
+        }
+
+        public IDatabase Database => _database;
+
+        public void CloseDatabase()
+        {
+            _connectionMultiplexer.Close();
+        }
+
+        public void FlushDatabase()
+        {
+            _connectionMultiplexer.GetServer(configurationString).FlushDatabase(databaseId);
+        }
+
+        private void CreateRedisConfigurationString()
+        {
+            configurationString = $"{host}:{port}";
+        }
+    }
+}
